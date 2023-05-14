4da0138458a10b75558f037833be3894fc5e1799
Xamana <gamingtest@mail.ru>
2023-05-14 23:09:00 +0300

    add styles

--- src/Components/pages/Login/index.jsx
+++ src/Components/pages/Login/index.jsx
@@ -2,49 +2,51 @@
 
 import React from 'react';
 import styles from './Login.module.css';
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
-import { getKeys } from '../../../redux/slice/loginSlice'
+import { getKeys } from '../../../redux/slice/loginSlice';
 
 export const Login = () => {
   const [idInstance, setIdInstance] = React.useState('');
   const [apiToken, setApiToken] = React.useState('');
   const dispatch = useDispatch();
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const authHandler = () => {
-    if(idInstance && apiToken) {
-      dispatch(getKeys({idInstance, apiToken}));
-      navigate('/chat')
-    }  
+    if (idInstance && apiToken) {
+      dispatch(getKeys({ idInstance, apiToken }));
+      navigate('/chat');
+    }
   };
   return (
-    <div className={styles.content}>
-      <div className={styles.inputWrapper}>
-        <div className={styles.inputText}>Введите idInstance</div>
-        <input
-          type='text'
-          placeholder='idInstance'
-          className={styles.input}
-          value={idInstance}
-          onChange={(e) => setIdInstance(e.target.value)}
-        />
-      </div>
-      <div className={styles.inputWrapper}>
-        <div className={styles.inputText}>Введите ApiTokenInstance</div>
-        <input
-          type='text'
-          placeholder='ApiTokenInstance'
-          className={styles.input}
-          value={apiToken}
-          onChange={(e) => setApiToken(e.target.value)}
-        />
-      </div>
+    <div className={styles.wrapper}>
+      <div className={styles.content}>
+        <div className={styles.inputWrapper}>
+          <div className={styles.inputText}>Введите idInstance</div>
+          <input
+            type='text'
+            placeholder='idInstance'
+            className={styles.input}
+            value={idInstance}
+            onChange={(e) => setIdInstance(e.target.value)}
+          />
+        </div>
+        <div className={styles.inputWrapper}>
+          <div className={styles.inputText}>Введите ApiTokenInstance</div>
+          <input
+            type='text'
+            placeholder='ApiTokenInstance'
+            className={styles.input}
+            value={apiToken}
+            onChange={(e) => setApiToken(e.target.value)}
+          />
+        </div>
 
-      <button className={styles.buttonSubmit} onClick={authHandler}>
-        Вход
-      </button>
+        <button className={styles.buttonSubmit} onClick={authHandler}>
+          Вход
+        </button>
+      </div>
     </div>
   );
 };
